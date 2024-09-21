bd8630d423447c7e4adc9a9b189f511026375301
MrMihen13 <kamerezhnikov@greenatom.ru>
2024-09-21 17:14:44 +0300

    Added grpc methods

--- migrations/1_200601021504_add_profile.sql
+++ migrations/1_200601021504_add_profile.sql
@@ -5,9 +5,9 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE profile
 (
     id         uuid UNIQUE PRIMARY KEY           DEFAULT uuid_generate_v4(),
+    email      varchar(254) UNIQUE      NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE          DEFAULT CURRENT_TIMESTAMP,
-    last_login TIMESTAMP WITH TIME ZONE
 );
 
 SELECT log_migration(1);
